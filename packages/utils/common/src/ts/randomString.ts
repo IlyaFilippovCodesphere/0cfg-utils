70ba232aaf597d015c22df1d46aeeeba1f394358
romfrolov <justcallmefrolov@gmail.com>
2021-04-29 11:01:40 +0300

    Reimplements randomString

--- packages/utils/common/src/ts/randomString.ts
+++ packages/utils/common/src/ts/randomString.ts
@@ -1,8 +1,16 @@
+const CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
 /**
- * Return a random string.
- * The string has a random length between 0 and 7 characters.
- * @returns {string} a string containing random characters.
+ * Returns a string of random characters with a custom length.
+ *
+ * @returns a string containing random characters.
  */
-export const randomString = (): string => {
-    return Math.random().toString(36).substring(0, 7);
+export const randomString = (length = 7): string => {
+    let str = '';
+
+    for (let i = 0; i < length; i++) {
+        str += CHARACTERS.charAt(Math.floor(Math.random() * CHARACTERS.length));
+    }
+
+    return str;
 };
