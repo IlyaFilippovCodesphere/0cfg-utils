557f7b6ce8c696088395ac71b675f61724963fb8
Elias Groll <elias.groll97@gmail.com>
2021-06-24 22:17:25 +0200

    fix lint errors

--- packages/utils/node/src/ts/security/HashHelper.ts
+++ packages/utils/node/src/ts/security/HashHelper.ts
@@ -1,7 +1,7 @@
 import {injectable} from 'inversify';
 import * as crypto from 'crypto';
 import {isA} from 'ts-type-checked';
-import {JsonValue} from "@0cfg/utils-common/lib/JsonValue";
+import {JsonValue} from '@0cfg/utils-common/lib/JsonValue';
 import objectHash from 'object-hash';
 
 export interface HashConfig {
