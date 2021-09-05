6ca3755b085490462bbdfda1152ddd4ba81b099e
Ilya Filippov <81969131+IlyaFilippovCodesphere@users.noreply.github.com>
2021-09-05 22:13:39 +0200

    Fix JsonValue typing for objects w/o properties
    Previous type only worked for objects with properties.
    i.e. 'hello' was not considered a valid JasonValue.

--- packages/utils/common/src/ts/JsonValue.ts
+++ packages/utils/common/src/ts/JsonValue.ts
@@ -2,13 +2,11 @@
  * This union mimics the types allowed in RFC 7159.
  * @see https://tools.ietf.org/html/rfc7159
  */
-export type JsonValue = {
-    [key in string | number]:
-        JsonValue
-        | Array<number | boolean | string | null | undefined | JsonValue>
-        | number
-        | boolean
-        | string
-        | null
-        | undefined;
-};
+export type JsonValue =
+    | string
+    | number
+    | boolean
+    | null
+    | undefined
+    | JsonValue[]
+    | {[key in string | number]: JsonValue};
